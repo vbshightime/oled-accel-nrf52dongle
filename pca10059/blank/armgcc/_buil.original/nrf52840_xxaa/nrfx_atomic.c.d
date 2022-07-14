4f8232a0ca61b0846243e9bbc955cdf8fe1e132c
vbshightime <vbsmandi@gmail.com>
2022-07-14 21:03:52 +0530

    first commit

---
+++ pca10059/blank/armgcc/_buil.original/nrf52840_xxaa/nrfx_atomic.c.d
@@ -0,0 +1,131 @@
+_build/nrf52840_xxaa/nrfx_atomic.c.o: \
+ ../../../../../../modules/nrfx/soc/nrfx_atomic.c \
+ ../../../../../../modules/nrfx/soc/nrfx_atomic.h \
+ ../../../../../../modules/nrfx/nrfx.h \
+ ../../../../../../integration/nrfx/nrfx_config.h ../config/sdk_config.h \
+ ../../../../../../modules/nrfx/drivers/nrfx_common.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../modules/nrfx/mdk/nrf_peripherals.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_peripherals.h \
+ ../../../../../../integration/nrfx/nrfx_glue.h \
+ ../../../../../../integration/nrfx/legacy/apply_old_config.h \
+ ../../../../../../modules/nrfx/soc/nrfx_irqs.h \
+ ../../../../../../modules/nrfx/soc/nrfx_irqs_nrf52840.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_util_platform.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdio.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/types.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/types.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/stdio.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../../../components/libraries/util/app_error_weak.h \
+ ../../../../../../modules/nrfx/soc/nrfx_coredep.h \
+ ../../../../../../modules/nrfx/soc/nrfx_atomic.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/libraries/util/sdk_resources.h \
+ ../../../../../../modules/nrfx/drivers/nrfx_errors.h \
+ ../../../../../../modules/nrfx/soc/nrfx_atomic_internal.h
+../../../../../../modules/nrfx/soc/nrfx_atomic.h:
+../../../../../../modules/nrfx/nrfx.h:
+../../../../../../integration/nrfx/nrfx_config.h:
+../config/sdk_config.h:
+../../../../../../modules/nrfx/drivers/nrfx_common.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+../../../../../../modules/nrfx/mdk/nrf.h:
+../../../../../../modules/nrfx/mdk/nrf52840.h:
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+../../../../../../components/toolchain/cmsis/include/cmsis_version.h:
+../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h:
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+../../../../../../components/toolchain/cmsis/include/mpu_armv7.h:
+../../../../../../modules/nrfx/mdk/system_nrf52840.h:
+../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h:
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h:
+../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h:
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+../../../../../../modules/nrfx/mdk/nrf_peripherals.h:
+../../../../../../modules/nrfx/mdk/nrf52840_peripherals.h:
+../../../../../../integration/nrfx/nrfx_glue.h:
+../../../../../../integration/nrfx/legacy/apply_old_config.h:
+../../../../../../modules/nrfx/soc/nrfx_irqs.h:
+../../../../../../modules/nrfx/soc/nrfx_irqs_nrf52840.h:
+../../../../../../components/libraries/util/nrf_assert.h:
+../../../../../../components/libraries/util/app_util.h:
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+../../../../../../components/libraries/util/nordic_common.h:
+../../../../../../components/libraries/util/nordic_common.h:
+../../../../../../components/libraries/util/app_util_platform.h:
+../../../../../../components/libraries/util/nrf_assert.h:
+../../../../../../components/libraries/util/app_error.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdio.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/types.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_pthreadtypes.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/types.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/stdio.h:
+../../../../../../components/libraries/util/sdk_errors.h:
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+../../../../../../components/libraries/util/app_error_weak.h:
+../../../../../../modules/nrfx/soc/nrfx_coredep.h:
+../../../../../../modules/nrfx/soc/nrfx_atomic.h:
+../../../../../../components/libraries/util/sdk_errors.h:
+../../../../../../components/libraries/util/sdk_resources.h:
+../../../../../../modules/nrfx/drivers/nrfx_errors.h:
+../../../../../../modules/nrfx/soc/nrfx_atomic_internal.h:
---
+++ pca10059/blank/armgcc/_build/nrf52840_xxaa/Font_8x10.c.d
@@ -0,0 +1,17 @@
+_build/nrf52840_xxaa/Font_8x10.c.o: ../../../ssd1306/Fonts/Font_8x10.c \
+ ../../../ssd1306/Fonts/fonts.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h
+../../../ssd1306/Fonts/fonts.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h:
+/home/vbshightime/nrf52/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h:
